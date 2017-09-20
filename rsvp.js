236ca41feb31c4ed39bc6304ac1b2c9c85dd2cb0
MEURANT <alexandremeurant@hotmail.fr>
2017-09-20 18:24:10 +0200

    Create a list item function

---
+++ rsvp.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
+
+function addLi() {
+    // create a list item:
+    const ListItem = document.createElement('li');
+    ListItem.textContent = input.value;
+    // create and add label with checkbox to the list item:
+    const label = document.createElement('label');
+    label.textContent = 'Confirmed';
+    const labelInput = document.createElement('input');
+    labelInput.type = 'checkbox';
+    label.appendChild(labelInput);
+    ListItem.appendChild(label);
+    // create and add "edit" button:
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    ListItem.appendChild(editButton);
+    // create and add "remove" button:
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    ListItem.appendChild(removeButton);
+    // Add the list item to the unorderes list:
+    ul.appendChild(ListItem);
+}
