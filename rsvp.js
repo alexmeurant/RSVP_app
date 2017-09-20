81b6458f94f95ec84e68b11db2e4f5a616b746fb
MEURANT <alexandremeurant@hotmail.fr>
2017-09-20 19:29:36 +0200

    Added remove button action

--- rsvp.js
+++ rsvp.js
@@ -6,6 +6,7 @@ function addLi() {
     // create a list item:
     const ListItem = document.createElement('li');
     ListItem.textContent = input.value;
+
     // create and add label with checkbox to the list item:
     const label = document.createElement('label');
     label.textContent = 'Confirmed';
@@ -13,15 +14,20 @@ function addLi() {
     labelInput.type = 'checkbox';
     label.appendChild(labelInput);
     ListItem.appendChild(label);
+
     // create and add "edit" button:
     const editButton = document.createElement('button');
+    editButton.className = 'edit';
     editButton.textContent = 'edit';
     ListItem.appendChild(editButton);
+
     // create and add "remove" button:
     const removeButton = document.createElement('button');
+    removeButton.className = 'remove';
     removeButton.textContent = 'remove';
     ListItem.appendChild(removeButton);
-    // Add the list item to the unorderes list:
+
+    // Add the list item to the unordered list:
     ul.appendChild(ListItem);
 }
 
@@ -31,4 +37,11 @@ form.addEventListener('submit', (e) => {
     // add list Item and empty input form:
     addLi();
     input.value = '';
+});
+
+ul.addEventListener('click', (event) => {
+    if (event.target.className === 'remove') {
+        const li = event.target.parentNode;
+        ul.removeChild(li);
+    }
 });
